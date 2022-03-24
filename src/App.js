dd8f32b83213fefbc9aa8629fcdda8b44b8400ae
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2022-03-25 01:06:39 +0600

    added basic routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Header from './Components/Header/Header';
 import { BrowserRouter as Router, Switch,Redirect, Route } from "react-router-dom";
+import Home from './Components/Home/Home';
 
 function App() {
   return (
     <Router>
       <Header></Header>
+      <Home/>
+
     </Router>
   );
 }
