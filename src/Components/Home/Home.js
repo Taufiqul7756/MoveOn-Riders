734d87beb3cd6a5776a6794517f643666dde36a1
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2022-03-25 01:05:37 +0600

    added home

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Category from '../Categories/Category';
+
+
+const Home = () => {
+
+    const vehicleName = [
+        {
+            title: 'Bike',
+            imgUrl: 'https://i.ibb.co/VB4JGVG/Frame.png',
+            capacity: 2,
+            vehicleType: 'bike',
+            price: 119
+        },
+        {
+            title: 'Car',
+            imgUrl: 'https://i.ibb.co/VB4JGVG/Frame.png',
+            capacity: 5,
+            vehicleType: 'car',
+            price: 119
+        },
+        {
+            title: 'Bus',
+            imgUrl: 'https://i.ibb.co/VB4JGVG/Frame.png',
+            capacity: 20,
+            vehicleType: 'bus',
+            price: 119
+        },
+        {
+            title: 'Train',
+            imgUrl: 'https://i.ibb.co/VB4JGVG/Frame.png',
+            capacity: 50,
+            vehicleType: 'train',
+            price: 119
+        }
+    ]
+    
+    return (
+        <div className="card-container">
+            {
+                vehicleName.map(vehicle =><Category vahicle={vehicle} />)
+            }
+            
+        </div>
+    );
+};
+
+export default Home;
