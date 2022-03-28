70f1a4500c4435436ae82104440146cdc1e8c30f
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2022-03-28 13:39:27 +0600

    Firebase config

---
+++ src/Components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDJ4IOI-MSmRg0bwNKdYP1aVVgWvY1ugOM",
+    authDomain: "moveon-riders.firebaseapp.com",
+    projectId: "moveon-riders",
+    storageBucket: "moveon-riders.appspot.com",
+    messagingSenderId: "125531975270",
+    appId: "1:125531975270:web:b0c632100fa2f8ab9c07a7"
+  };
+
+  export default firebaseConfig;
