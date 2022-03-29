be2a747738dc8c18fd3422ded0a9230515d056e6
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2022-03-29 15:22:08 +0600

    Destination Page design

--- src/Components/Destination/Destination.js
+++ src/Components/Destination/Destination.js
@@ -1,14 +1,33 @@
 import React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+
 import "../Destination/Destination.css";
 
 const Destination = () => {
   return (
     <div className="destination-container">
       <div className="search-container">
-        <h1>Destination</h1>
+        <Card sx={{ minWidth: 275 }}>
+          <CardContent>
+            <h4>Pick From</h4>
+            <TextField id="outlined-basic" label="From*" variant="outlined" />
+            <h4>Pick To</h4>
+            <TextField id="outlined-basic" label="To*" variant="outlined" />
+          </CardContent>
+          <CardActions>
+            <Button variant="contained" size="small">
+              Search
+            </Button>
+          </CardActions>
+        </Card>
       </div>
-      <div className="map-container">
-        <h1>Destination</h1>
+
+      <div className="right-container">
+        <img src="https://i.ibb.co/4dZDPDy/Map.png" alt="" />
       </div>
     </div>
   );
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -15,21 +15,21 @@ const Home = () => {
         },
         {
             title: 'Car',
-            imgUrl: 'https://i.ibb.co/VB4JGVG/Frame.png',
+            imgUrl: 'https://i.ibb.co/7zCNRdY/Frame-2.png" alt="Frame-2" border="0"',
             capacity: 5,
             vehicleType: 'car',
             price: 119
         },
         {
             title: 'Bus',
-            imgUrl: 'https://i.ibb.co/VB4JGVG/Frame.png',
+            imgUrl: 'https://i.ibb.co/MSY9PnW/Frame-1.png" alt="Frame-1" border="0"',
             capacity: 20,
             vehicleType: 'bus',
             price: 119
         },
         {
             title: 'Train',
-            imgUrl: 'https://i.ibb.co/VB4JGVG/Frame.png',
+            imgUrl: 'https://i.ibb.co/vQVhNXm/Group.png" alt="Group" border="0"',
             capacity: 50,
             vehicleType: 'train',
             price: 119
