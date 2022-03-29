26560c905bd6b73e6697b5c49ef0a4b82f81855e
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2022-03-29 22:27:31 +0600

    google signin button design

--- src/Components/Login/SignUp.js
+++ src/Components/Login/SignUp.js
@@ -1,19 +1,18 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 // import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import '../Login/SignUp.css'
-
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import "../Login/SignUp.css";
 
 import firebaseConfig from "./firebase.config";
 import { initializeApp } from "firebase/app";
@@ -27,23 +26,20 @@ import { UserContext } from "../../App";
 import { useHistory, useLocation } from "react-router-dom";
 import { useContext } from "react";
 
-
 const theme = createTheme();
 
-const SignUp =() => {
-
+const SignUp = () => {
   const [loggedInUser, setloggedInUser] = useContext(UserContext);
   const history = useHistory();
   const location = useLocation();
-  const { from } = location.state || { from: { pathname: "" } };
+  const { from } = location.state || { from: { pathname: "/" } };
   const app = initializeApp(firebaseConfig);
 
   const handleClick = () => {
     console.log("Clicked");
-    let path = "/login"; 
+    let path = "/login";
     history.push(path);
-    
-  }
+  };
 
   const handleGoogleSignIn = () => {
     const googleProvider = new GoogleAuthProvider();
@@ -69,41 +65,39 @@ const SignUp =() => {
       });
   };
 
-
-
-
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
-      email: data.get('email'),
-      password: data.get('password'),
+      email: data.get("email"),
+      password: data.get("password"),
     });
   };
 
   return (
-    <ThemeProvider  theme={theme}>
-      <div>
-        <button onClick={handleGoogleSignIn}>Google Sign in</button>
-        
-      </div>
+    <ThemeProvider theme={theme}>
       <Container className="signup-container" component="main" maxWidth="xs">
         <CssBaseline />
         <Box
           sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             {/* <LockOutlinedIcon /> */}
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign up
           </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
             <Grid container spacing={2}>
               <Grid item xs={12} sm={6}>
                 <TextField
@@ -149,7 +143,9 @@ const SignUp =() => {
               </Grid>
               <Grid item xs={12}>
                 <FormControlLabel
-                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  control={
+                    <Checkbox value="allowExtraEmails" color="primary" />
+                  }
                   label="I want to receive inspiration, marketing promotions and updates via email."
                 />
               </Grid>
@@ -164,16 +160,25 @@ const SignUp =() => {
             </Button>
             <Grid container justifyContent="flex-end">
               <Grid item>
-                <Link className="cursor"  onClick={handleClick} variant="body2">
+                <button
+                  onClick={handleGoogleSignIn}
+                  type="button"
+                  class="login-with-google-btn"
+                >
+                  Sign in with Google
+                </button>
+               
+                <Link className="cursor" onClick={handleClick} variant="body2">
                   Already have an account? Sign in
                 </Link>
               </Grid>
             </Grid>
           </Box>
         </Box>
+        <div></div>
       </Container>
     </ThemeProvider>
   );
-}
+};
 
 export default SignUp;
